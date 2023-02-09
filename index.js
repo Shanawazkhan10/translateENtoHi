7cb818629c2ca4c9e0414d77a7a00ac884bfed72
shanawaz.khan <shanawaz.khan@nivesh.com>
2023-02-09 14:20:29 +0530

    by use google API

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ Object.keys(data).forEach(async function (key) {
     const translatedText = await translate(data[key], { from: 'en', to: 'hi' });
     console.log(`"${key}"` + ":" + `"${translatedText}",`); // こんにちは世界 
 })
+
+// const axios = require('axios');
+
+// async function translateEnglishToHindi(text) {
+//   const result = await axios.get('https://translate.googleapis.com/translate_a/single', {
+//     params: {
+//       client: 'gtx',
+//       sl: 'en',
+//       tl: 'hi',
+//       dt: 't',
+//       q: text
+//     }
+//   });
+
+//   return result.data[0][0][0];
+// }
+
+// translateEnglishToHindi('hello').then(translation => {
+//   console.log(translation);
+// });
